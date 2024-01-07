54de37b1229a4a1eb86f400316489395e92dc511
Yuri <yuriyuri200117@gmail.com>
2024-01-08 00:50:10 +0100

    Initial Commit

---
+++ AirBnb/AirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbApp.swift
+//  AirBnb
+//
+//  Created by Yuri Petrosyan on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
